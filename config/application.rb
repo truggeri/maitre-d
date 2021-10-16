abd06a0813de3af39f4eb8a22cf98a6f85bfafbc
Thomas Ruggeri <truggeri@gmail.com>
2021-10-16 17:05:10 -0400

    rubocop: Space in parens

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+Bundler.require( *Rails.groups )
 
 module MaitreD
   class Application < Rails::Application
