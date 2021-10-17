67925889576cfe94f0292828d13257d2bd4042ed
Thomas Ruggeri <truggeri@gmail.com>
2021-10-17 12:43:00 -0400

    Fixing flaky spec in CI

--- spec/lib/token_spec.rb
+++ spec/lib/token_spec.rb
@@ -5,9 +5,10 @@ require "rails_helper"
 RSpec.describe "Token" do
   let( :instance ) { Token.new roles: given_roles, id: uuid }
   let( :uuid ) { "c883acd1-5064-4349-87ad-60d4a63f8dca" }
+  let( :frozen_time ) { Time.parse( "2021-10-16 08:24:12Z" ) }
 
   around( :example ) do | example |
-    Timecop.freeze( 2021, 10, 16 ) do
+    Timecop.freeze( frozen_time ) do
       example.run
     end
   end
@@ -19,12 +20,9 @@ RSpec.describe "Token" do
       let( :given_roles ) { [] }
 
       it do
-        jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJwdWJsaWMiLCJleHAiOjE2MzQzNDYwMDAsImlhdCI6MTYzNDM0MjQ" \
-              "wMCwiaXNzIjoibWFpdHJlLWQiLCJqdGkiOiJjODgzYWNkMS01MDY0LTQzNDktODdhZC02MGQ0YTYzZjhkY2EiLCJyb2xlcyI6W119." \
-              "c64d6VNSMweCbmqBgEjppecJMVkJXsGV2bCvvp13njKkvKBur7bHMkHIeoyda3MM4VpNFNNfb-4SGKoi3rEidNtztrj5dzJTVoDCH" \
-              "-gkR9n5xv2bK23D-X5bIe3s4oypwy9YCLE262MbFUYqEyiQY_IltseITCdU2jSAfIlxcDsNoZJjwwULwYiwq-Y6QTCx4n9IEUn-" \
-              "eso05avjtho_It2LbFnArAI3JaHOIQuMrjwAk18UwuBV67knQ4wsT_YBP5i4NpMlwuu-SZplpx0sFkP96pBQgTwdV_9qUvlor1-" \
-              "WLv04YDEC8pP4Ntk7BAJMqMkFr60dY05GubxELb052g"
+        # rubocop:disable Layout/LineLength
+        jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJwdWJsaWMiLCJleHAiOjE2MzQzNzYyNTIsImlhdCI6MTYzNDM3MjY1MiwiaXNzIjoibWFpdHJlLWQiLCJqdGkiOiJjODgzYWNkMS01MDY0LTQzNDktODdhZC02MGQ0YTYzZjhkY2EiLCJyb2xlcyI6W119.aapd0uuZ8ay43r4RmuCW3I67S3FoRg9WS_1TWMrHEISXHH1PBUnrPkWDueBB8XtPG453yyxjCyEl3osQjiiIe1Ge59bePyQF-58egAEBFlig39DvuYto5dz4tYPnxff164BY4Mzqlvs9Dr3V7BkFqmop5EaXebOy_dctrlzhkkfuwIpsgwEWOyqhMDR5jjrPC-neawmGbwc_KrT7fJOzEmcS33gSFLOltF_6bVWBwF7k5nJMRefqak3GB_K0dnQtNtxy3IPac5b6Dhci42iuAw8HL7ZUY_BwAOigopA9HM_9HeXtvdypgBz-TJpynH2-FVz-gX7htwGTNmbRX8m6yw"
+        # rubocop:enable Layout/LineLength
         expect( subject ).to eq jwt
       end
     end
@@ -33,12 +31,9 @@ RSpec.describe "Token" do
       let( :given_roles ) { [ :sys_admin, :master_chef ] }
 
       it do
-        jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJwdWJsaWMiLCJleHAiOjE2MzQzNDYwMDAsImlhdCI6MTYzNDM0MjQ" \
-              "wMCwiaXNzIjoibWFpdHJlLWQiLCJqdGkiOiJjODgzYWNkMS01MDY0LTQzNDktODdhZC02MGQ0YTYzZjhkY2EiLCJyb2xlcyI6WyJz" \
-              "eXNfYWRtaW4iLCJtYXN0ZXJfY2hlZiJdfQ.mstJrQNPLuWuNE9BF_Rf1v3jVUqB_r9Mu_jpNQNdHV-L9mRSCyh4BR7OrZrSZKS3kjD" \
-              "5bI-RzKZb3SJkyyW6eT3Xw1PXl_Z362WhMfpWUUk801l9rW5s7rPNXm7mlmDAvwFUVUu1iu9RcRlW2x9O0IDbuin6AW5hNjeI4mYPw" \
-              "StJCc28NWxMglTCIS1Mw3jEzCD5Il9oHtv4OSenaBVvreqYxSILeFevhVixVVd1N3p7StOE0zggVoN11nnF5eZsHO3dfNULUT2ZXl" \
-              "OzP_wtJi2o7Q80j1x7hceX8binTQtnICSeORnOuwmoyepUKRD4OIvmq4nVEEYYxLGPuRHD9w"
+        # rubocop:disable Layout/LineLength
+        jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJwdWJsaWMiLCJleHAiOjE2MzQzNzYyNTIsImlhdCI6MTYzNDM3MjY1MiwiaXNzIjoibWFpdHJlLWQiLCJqdGkiOiJjODgzYWNkMS01MDY0LTQzNDktODdhZC02MGQ0YTYzZjhkY2EiLCJyb2xlcyI6WyJzeXNfYWRtaW4iLCJtYXN0ZXJfY2hlZiJdfQ.aXVgzZSj19v2GW2fWM7CNVIHZS_rSsSk5Nm8NkwIJ7dXY9dPgMHBhsxe7vyZBtrXL57hiOeub2V1B98f-SGr_JjL_3bAXYOZctDG1wLbiqwaXRTY68n86x0KSm3fcqPCaqKgIXNFMUaYr9F2pmXDLkuN7-6TYE2Z_zjYHLbEvfAzi_QLqeZuSbv5OrzUGdBAcIJ5_RFTOtcfjnh6mBboiV4c3cOEUVpprVAHQToQvuVbMOUMa4g4dWyOUQzzAwH_NR_EmG4eSXBZ6nQjTuidd1qIQ_YUb28z85_oFfWy2E50Nf1Zlbh8WlEh9CAVbGZeANEmVyII4oBVAiA-_QqaXA"
+        # rubocop:enable Layout/LineLength
         expect( subject ).to eq jwt
       end
 
@@ -48,12 +43,9 @@ RSpec.describe "Token" do
 
         it "uses generated uuid" do
           allow( SecureRandom ).to receive( :uuid ).and_return generated_string
-          jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJwdWJsaWMiLCJleHAiOjE2MzQzNDYwMDAsImlhdCI6MTYzNDM0" \
-                "MjQwMCwiaXNzIjoibWFpdHJlLWQiLCJqdGkiOiJzb21lLWdlbmVyYXRlZC1zdHJpbmciLCJyb2xlcyI6WyJzeXNfYWRtaW4iL" \
-                "CJtYXN0ZXJfY2hlZiJdfQ.eN9-w_VpQvZcFGW3KMckPZXrY7IzsjB2aPWr0jeLWFqeqHoRphIND3B4lxGHHuPt3xKa9TOjpow" \
-                "BZMYP43QDguFd2kmrcZ1QEXPZ_aTeMcsbXN8xMEM2hNCI7Du2aM0tKFdk0seVccb2p_zSf4gEv8rTOXdIp7DWCHGUXk19IdmH" \
-                "A7wucqQuEoDlQ-ffYSJlBmeXKtmdvS7C5jPIlkv4fbyH9sljvBdBzGeuKtIwqXubpHOUBHLaKoqP5Igo9fAXTUjDrTuefTby2" \
-                "erjs0aWJK738VvwkB3nimoJ4WOCrk4bKXZlNE3SRi7AiTITCcKjFqYejLtHHF4Orr0LeGy0ew"
+          # rubocop:disable Layout/LineLength
+          jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJwdWJsaWMiLCJleHAiOjE2MzQzNzYyNTIsImlhdCI6MTYzNDM3MjY1MiwiaXNzIjoibWFpdHJlLWQiLCJqdGkiOiJzb21lLWdlbmVyYXRlZC1zdHJpbmciLCJyb2xlcyI6WyJzeXNfYWRtaW4iLCJtYXN0ZXJfY2hlZiJdfQ.dyfYMoS12sv754HyPuUx3DtY8YQnjUIeM8kOxhR0EkDvCA3xVmJHhgXah_5Cj5s_PfKPlzV5DmII57p26SxzsmjED329irqysYWEuolJxdwlTBe-1Z9NaR2WlxX--C4-ecZF49kXYnvDbjNq5yqsi6gMJXWmGvZkSH9D8QmhLAdMBOpHVg6zxM2ESnzLZ03qsSBz4nkpgGbQ47tlhyFXynBKDp5U-T83DCzlQlhnUV2jfNp6bpNv0kAtGQHWwH5h76gPEBZoAqZr386UQYNjY2NuPJ5rZp3k49YiSJoq0cn_nIKItQMXAvWSRw3WY9EJjDVMaAK6PjbacHpVAhpXvA"
+          # rubocop:enable Layout/LineLength
           expect( subject ).to eq jwt
         end
       end
